b4c96a27fc36fa17b8192763e398cb69086d3b63
Karan3872 <karananuragi7879@gmail.com>
2025-01-08 12:23:29 +0530

    added program about area of circle

---
+++ Circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Circle{
+  public static void main(String args[]){
+	  Scanner sc = new Scanner(System.in);
+     System.out.println("enter radius of the circle");
+	 int a;
+	 a=sc.nextInt();
+	 System.out.println(3.14*(a*a));
+  }
+}
