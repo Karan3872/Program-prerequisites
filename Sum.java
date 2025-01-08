104d7165bb2103932b597ca060295bab7a2c1b7b
Karan3872 <karananuragi7879@gmail.com>
2025-01-08 11:54:27 +0530

    added program about add to numbers

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sum{
+  public static void main(String args[]){
+	  Scanner sc = new Scanner(System.in);
+     System.out.println("enter no 1");
+	 int a;
+	 a=sc.nextInt();
+	 System.out.println("enter no 2");
+	 int b;
+	 b=sc.nextInt();
+	 System.out.println(a+b);
+	 
+  }
+}
